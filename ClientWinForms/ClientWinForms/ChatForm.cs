c0c71a833c9a759091ffc90f82ed8080ef36eccd
Anna Androshchuk <annaandroshchuk91@gmail.com>
2025-05-28 09:52:30 +0300

    Added ClientWinForms folder

---
+++ ClientWinForms/ClientWinForms/ChatForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWinForms
+{
+    public partial class ChatForm : Form
+    {
+
+        private TcpClient client; //TCP з’єднання з сервером
+        private NetworkStream stream; //потік для відправки повідомлень
+        private string username; //нейм користувача
+
+
+        //1. Конструктор ChatForm. Викликається одразу після входу в LoginForm,
+        //запускає прослуховування повідомлення через функцію Task.Run(ReceiveLoopAsync)
+        public ChatForm(TcpClient client, NetworkStream stream, string username)
+        {
+            InitializeComponent();
+            this.client = client;
+            this.stream = stream;
+            this.username = username;
+
+            Task.Run(ReceiveAsync);
+
+            // додані для тесту
+
+            lstUser.Items.Add("user1"); 
+            lstUser.Items.Add("user2");
+        }
+
+
+        //2. Метод btnSendMessage_Click. Виконується при натисканні кнопки Send message. 
+        //- перевірка на те, чи обраний юзер, якому відправляють повідомлення
+        //- зчитування повідомлення з txtbMessage, і перевірка щоб повідомлення було не порожнє
+        //- утворення нового повідомлення через SendAsync
+        //- додавання меседжів в історію чату
+
+        private async void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            if (lstUser.SelectedItem == null)
+            {
+                MessageBox.Show("Choose a user");
+                return;
+            }
+
+            string receiver = lstUser.SelectedItem.ToString();
+            string text = txtbMessage.Text;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                MessageBox.Show("Write a message");
+                return;
+            }
+
+            string message = $"{receiver}\n{text}";
+            await SendAsync(message);
+
+
+            AddChatHistory($"{receiver}: {text}");
+
+            txtbMessage.Clear();
+        }
+
+        //3.  Метод SendAsync. Перетворює повідомлення у байти і відправляє їх у потік
+        private async Task SendAsync(string msg)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(msg + "\n");
+            await stream.WriteAsync(data, 0, data.Length);
+        }
+
+
+        //4. Метод ReceiveAsync. Цикл, який читає повідомлення з сервера в буфер,
+        //якщо повідомлення не прийшло, або порожнє, цикл переривається,
+        //якщо повідомлення прийшло, перетворює масив байтів у string, 
+        //і додає нове повідомлення в історію чату
+        private async Task ReceiveAsync()
+        {
+            try
+            {
+                byte[] buf = new byte[1024];
+                while (true)
+                {
+                    int bytesRead = await stream.ReadAsync(buf, 0, buf.Length);
+                    if (bytesRead <= 0) break;
+
+                    string message = Encoding.UTF8.GetString(buf, 0, bytesRead);
+
+
+                    Invoke(() => AddChatHistory($"{message}"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke(() => AddChatHistory(ex.Message));
+            }
+        }
+
+        //5. Метод AddChatHistory. Додає новий меседж в історію чату txtbHistory.
+        // ScrollToCaret - означає, що у текст-боксу є автоматичн прокрутка до низу
+        private void AddChatHistory(string text)
+        {
+            txtbHistory.AppendText(text + Environment.NewLine);
+            txtbHistory.SelectionStart = txtbHistory.Text.Length;
+            txtbHistory.ScrollToCaret();
+        }
+
+
+    }
+}
---
+++ ClientWinForms/ClientWinForms/LoginForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ClientWinForms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picbIcon = new PictureBox();
+            lblogin = new Label();
+            txtbLogin = new TextBox();
+            txtbPassword = new TextBox();
+            btnLogin = new Button();
+            ((System.ComponentModel.ISupportInitialize)picbIcon).BeginInit();
+            SuspendLayout();
+            // 
+            // picbIcon
+            // 
+            picbIcon.Image = Properties.Resources.vectorstock_green_icon;
+            picbIcon.Location = new Point(96, 9);
+            picbIcon.Name = "picbIcon";
+            picbIcon.Size = new Size(237, 162);
+            picbIcon.SizeMode = PictureBoxSizeMode.Zoom;
+            picbIcon.TabIndex = 0;
+            picbIcon.TabStop = false;
+            // 
+            // lblogin
+            // 
+            lblogin.AutoSize = true;
+            lblogin.Font = new Font("Segoe UI", 19.5F, FontStyle.Bold, GraphicsUnit.Point, 1, true);
+            lblogin.ForeColor = SystemColors.GrayText;
+            lblogin.Location = new Point(30, 167);
+            lblogin.Name = "lblogin";
+            lblogin.Size = new Size(364, 45);
+            lblogin.TabIndex = 1;
+            lblogin.Text = "Login to Your Account";
+            // 
+            // txtbLogin
+            // 
+            txtbLogin.Location = new Point(36, 242);
+            txtbLogin.Name = "txtbLogin";
+            txtbLogin.PlaceholderText = "Login";
+            txtbLogin.Size = new Size(350, 27);
+            txtbLogin.TabIndex = 2;
+            // 
+            // txtbPassword
+            // 
+            txtbPassword.Location = new Point(36, 294);
+            txtbPassword.Name = "txtbPassword";
+            txtbPassword.PasswordChar = '*';
+            txtbPassword.PlaceholderText = "Password";
+            txtbPassword.Size = new Size(350, 27);
+            txtbPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.SeaGreen;
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Font = new Font("Segoe UI", 12F);
+            btnLogin.ForeColor = SystemColors.Window;
+            btnLogin.Location = new Point(36, 353);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(350, 45);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(437, 428);
+            Controls.Add(btnLogin);
+            Controls.Add(txtbPassword);
+            Controls.Add(txtbLogin);
+            Controls.Add(lblogin);
+            Controls.Add(picbIcon);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)picbIcon).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picbIcon;
+        private Label lblogin;
+        private TextBox txtbLogin;
+        private TextBox txtbPassword;
+        private Button btnLogin;
+    }
+}
